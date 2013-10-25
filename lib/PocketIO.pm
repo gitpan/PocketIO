88a13aa10dcaf846d9ffa4ddbf0111972497d0e0
Вячеслав Тихановский <vti@cpan.org>
2013-10-25 16:56:13 +0000

    Import of VTI/pocketio-0.16 from CPAN.
    gitpan-cpan-distribution: PocketIO
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         VTI/pocketio-0.16.tar.gz
    gitpan-cpan-author:       VTI
    gitpan-cpan-maturity:     released

--- lib/PocketIO.pm
+++ lib/PocketIO.pm
@@ -3,7 +3,7 @@ package PocketIO;
 use strict;
 use warnings;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use overload '&{}' => sub { shift->to_app(@_) }, fallback => 1;
 
@@ -95,7 +95,7 @@ sub _get_handler {
         $class->new;
       };
 
-    return $instance->run;
+    return $instance->$method;
 }
 
 1;
@@ -351,13 +351,17 @@ Michael FiG
 
 Peter Stuifzand
 
+tokubass
+
+mvgrimes
+
 =head1 AUTHOR
 
 Viacheslav Tykhanovskyi, C<vti@cpan.org>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2012, Viacheslav Tykhanovskyi
+Copyright (C) 2011-2013, Viacheslav Tykhanovskyi
 
 This program is free software, you can redistribute it and/or modify it under
 the terms of the Artistic License version 2.0.
